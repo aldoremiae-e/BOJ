b8c5f28f9d1259c8335f55c053aced22eeff3f0d
miae <97646732+aldoremiae-e@users.noreply.github.com>
2022-10-30 19:55:19 +0900

    [Silver II] Title: 쇠막대기, Time: 132 ms, Memory: 116056 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.py
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -1,24 +1,19 @@
 import sys
-from collections import deque
 bracket = list(sys.stdin.readline())
-stack = deque([])
 cnt = 0
 ans = 0
 for i in range(len(bracket)):
     if bracket[i] == '(':
         cnt += 1
-        stack.append(bracket[i])
 
     elif bracket[i] == ')':
         if bracket[i-1] == '(':
             # i-1과 i는 레이저
-            stack.pop()
             cnt -= 1
             ans += cnt
 
         else:
             # 닫힌괄호
-            stack.pop()
             ans += 1
             cnt -= 1
 
