27300a514999c2be04ed72e5c15e63da6fa38326
miae <97646732+aldoremiae-e@users.noreply.github.com>
2022-11-04 00:09:47 +0900

    [Silver III] Title: 소수 구하기, Time: 172 ms, Memory: 123200 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,12 @@
-m,n=map(int,input().split())
+m, n = list(map(int, input().split()))
+lst = [1 for _ in range(n+1)]
+lst[0] = 0
+lst[1] = 0
 
-for i in range(m,n+1):
-    if i==1:
-        continue
-    for j in range(2,int(i**0.5)+1):
-        if i%j==0:
-            break
-    else:
-        print(i)
+for i in range(2, n+1):
+    if lst[i]:
+        for j in range(i*i, n+1, i):
+            lst[j] = 0
+for k in range(m, n+1):
+    if lst[k] == 1:
+        print(k)
