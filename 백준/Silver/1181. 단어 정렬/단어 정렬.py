0335ab8f87564fb756bb8a4a2b84a3cd5337bf10
miae <97646732+aldoremiae-e@users.noreply.github.com>
2022-11-05 15:04:33 +0900

    [Silver V] Title: 단어 정렬, Time: 212 ms, Memory: 119812 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,17 +1,10 @@
-from collections import deque
 import sys
 N = int(input())
-dic = deque([])
+setlst = set()
 
 for _ in range(N):
-    dic.append(sys.stdin.readline().rstrip())
-# 중복을 없애줌
-setlst = set(dic)
-sortlst = []
-for i in setlst:
-    sortlst.append((len(i), i))
-# 숫자순, 알파벳순으로 정렬
-sortlst= sorted(sortlst)
-
-for j in sortlst:
-    print(j[1], end='\n')
+    s = sys.stdin.readline().rstrip()
+    setlst.add((len(s), s))
+setlst = sorted(setlst)
+for l in setlst:
+    print(l[1])
