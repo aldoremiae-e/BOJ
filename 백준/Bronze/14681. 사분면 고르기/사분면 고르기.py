7056ddd650ac13d0a4e736652745115a8f276ee7
miae <97646732+aldoremiae-e@users.noreply.github.com>
2022-12-22 22:36:05 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,11 @@
+from sys import stdin
+x = int(stdin.readline())
+y = int(stdin.readline())
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+elif x > 0 and y < 0:
+    print(4)
