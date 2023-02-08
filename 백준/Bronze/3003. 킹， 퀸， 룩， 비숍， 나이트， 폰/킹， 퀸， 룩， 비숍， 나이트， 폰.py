52119d132aaedc3c55c1742420911bf78b1fd2ea
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-02-08 21:10:42 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,8 @@
+from sys import stdin
+lst = list(map(int, stdin.readline().split()))
+print(1-lst[0], end=' ')
+print(1-lst[1], end=' ')
+print(2-lst[2], end=' ')
+print(2-lst[3], end=' ')
+print(2-lst[4], end=' ')
+print(8-lst[5], end=' ')
