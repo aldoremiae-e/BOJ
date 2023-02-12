984ddf9853f12c37d2af15b2a3f1fad351be2880
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-02-12 18:40:04 +0900

    [Silver III] Title: 01타일, Time: 124 ms, Memory: 122144 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+N = int(stdin.readline())
+dp = [0] * (N+1)
+if N == 1:
+    dp[0] = 0
+    dp[1] = 1
+if N == 2:
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+if N >= 3:
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, N+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 15746
+print(dp[N])
