37cd89f4ca72fef49e6a3c341bd132a1ceaf8714
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-02-23 16:53:03 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 140 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,14 +1,10 @@
 from sys import stdin
-from collections import deque
 N = int(stdin.readline())
-arr = list(map(int, stdin.readline().split()))
-# 임의의 수열이 주어질 때, 수열에서 몇 개의 수를 제거하여 부분 수열을 만들 수 있다.
-# arr[i]를 마지막 원소로 가질 때의 가장 긴 증가하는 수열의 크기 , 1로 초기화
-dp = [1] * N
+lst = list(map(int, stdin.readline().split()))
 
-for i in range(N):
+dp = [1] * N
+for i in range(1, N):
     for j in range(i):
-        if arr[i] > arr[j]:
-            if dp[j]+1 > dp[i]:
-                dp[i] = dp[j] + 1
+        if lst[i] > lst[j]:
+            dp[i] = max(dp[j]+1, dp[i])
 print(max(dp))
