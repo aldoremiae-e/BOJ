93f3383809671a18c8f982d576d83d6f07ae0655
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-05-12 21:10:46 +0900

    [level 2] Title: 더 맵게, Time: 1599.95 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,13 +1,13 @@
-from heapq import heappop, heappush, heapify
-def solution(scoville, K):
+from heapq import heappop, heappush
+def solution(scov, K):
     answer = 0
     heap = []
-    for i in scoville:
+    for i in scov:
         heappush(heap, i)
     while heap[0] < K:
-        try:
-            heappush(heap, heappop(heap) + heappop(heap) * 2)
-        except IndexError:
-            return -1
+        n = heappop(heap) + (heappop(heap) * 2)
+        heappush(heap, n)
         answer += 1
+        if len(heap) == 1 and heap[0] < K:
+            return -1
     return answer
