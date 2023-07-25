51ca6629bfeb820c7b245068d8b0ea03e9afa457
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-07-26 01:36:07 +0900

    [Gold II] Title: 어른 상어, Time: 272 ms, Memory: 118136 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.py
+++ 백준/Gold/19237. 어른 상어/어른 상어.py
@@ -1,7 +1,7 @@
-from heapq import heappop, heappush
+from collections import deque
 N, M, k = map(int, input().split())
 maps = [[0] * N for _ in range(N)]
-heap = []
+q = deque()
 # 상어의 위치 -> dict
 shark_ij = {}
 for i in range(N):
@@ -14,7 +14,7 @@ for i in range(N):
 # 상어의 방향 ->list
 shark_dir = list(map(int, input().split()))
 for i in range(1, M+1):
-    heappush(heap, (i, shark_dir[i-1]-1, shark_ij[i][0], shark_ij[i][1]))
+    q.append((i, shark_dir[i-1]-1, shark_ij[i][0], shark_ij[i][1]))
 
 # 상어의 우선순위 -> dict{n:[방향우선순위]}
 shark_dict = {}
@@ -22,11 +22,11 @@ for i in range(1, M+1):
     shark_dict[i] = [[] for _ in range(4)]
     for j in range(4):
         shark_dict[i][j] = list(map(int, input().split()))
-def next_heap(heap):
-    nq = []
-    while heap:
+def next_q(q):
+    nq = deque()
+    while q:
         # 번호, 방향, 좌표
-        sn, sd, si, sj = heappop(heap)
+        sn, sd, si, sj = q.popleft()
         # 방향 우선순위
         dirs = shark_dict[sn][sd]
         # 다음방향이 될 것
@@ -50,19 +50,15 @@ def next_heap(heap):
             if maps[ni][nj] == 0:
                 flag = True
                 next_d, next_i, next_j = dirs[idx]-1, ni, nj
-        #        heappush(next_ij, (0, idx, ni, nj))
             elif maps[ni][nj] // 1000 == maps[si][sj] // 1000:
                 if next_d == -1:
                     next_d, next_i, next_j = dirs[idx]-1, ni, nj
-        #        heappush(next_ij, (1, idx, ni, nj))
-        #next_d, next_i, next_j = dirs[next_ij[0][1]] - 1, next_ij[0][2], next_ij[0][3]
-        #nq.append((sn, next_d, next_i, next_j))
         nq.append((sn, next_d, next_i, next_j))
     return nq
 
 time = 1
 while time < 1001:
-    heap = next_heap(heap)
+    q = next_q(q)
     for i in range(N):
         for j in range(N):
             if maps[i][j] == 0:
@@ -70,9 +66,9 @@ while time < 1001:
             maps[i][j] -= 1
             if maps[i][j] % 1000 == 0:
                 maps[i][j] = 0
-    nq = []
-    while heap:
-        n, d, i, j = heappop(heap)
+    nq = deque()
+    while q:
+        n, d, i, j = q.popleft()
         # 빈곳이거나, 자기냄새있는 곳으로 갔을 때
         if maps[i][j] == 0 or maps[i][j] // 1000 == n:
             maps[i][j] = n * 1000 + k
@@ -80,7 +76,7 @@ while time < 1001:
     if len(nq) == 1:
         break
     else:
-        heap = nq
+        q = nq
         time += 1
 if time < 1001:
     print(time)
