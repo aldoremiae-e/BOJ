186d4bd0f599d6fb1b6f2f5149efefb5409031c1
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-07-26 17:39:51 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 580 ms, Memory: 262140 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,20 @@
+N = int(input())
+lst = list(map(int, input().split()))
+
+memo = [-1000000001]
+for num in lst:
+    if memo[-1] < num:
+        memo.append(num)
+    else:
+        left = 0
+        right = len(memo)
+        # 이분탐색을 통한 lower_bound
+        while left < right:
+            mid = (left + right) // 2
+            if memo[mid] < num:
+                left = mid + 1
+            else:
+                right = mid
+        memo[left] = num
+    # 배열에 들어있는 값들은 LIS를 이루는 요소와 무관하다
+print(len(memo)-1)
