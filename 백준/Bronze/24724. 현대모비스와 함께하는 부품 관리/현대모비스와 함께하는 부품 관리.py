73bc4a335f68ff7fa52c09e314a4efe2021a66de
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-08-04 18:08:05 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 284 ms, Memory: 116472 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,8 @@
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    A, B = map(int, input().split())
+    for _ in range(N):
+        a, b = map(int, input().split())
+    print(f'Material Management {tc}')
+    print('Classification ---- End!')
