2bdec5c4b05ccf0f5fcf0eb0969c2c75c75369b1
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-08-29 22:19:03 +0900

    [Gold IV] Title: 치즈, Time: 164 ms, Memory: 116572 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,81 +1,41 @@
 from collections import deque
 
 N, M = map(int, input().split())
-
 maps = [list(map(int, input().split())) for _ in range(N)]
 
-# 공기
-def airs(i, j):
-    global maps
+def bfs():
+    global ans, time
     q = deque()
-    q.append((i, j))
-    v = [[0] * M for _ in range(N)]
-    v[i][j] = 1
-    maps[i][j] = 2
+    q.append((0, 0))
+    visit = [[0] * M for _ in range(N)]
+    visit[0][0] = 1
+    dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    chz = deque()
     while q:
-        ci, cj = q.popleft()
+        i, j = q.popleft()
         for di, dj in dirs:
-            ni, nj = ci + di, cj + dj
+            ni, nj = i + di, j + dj
             if not (0 <= ni < N and 0 <= nj < M):
                 continue
-            if v[ni][nj] == 1 or maps[ni][nj] == 1:
-                continue
-            if maps[ni][nj] == 0: # 외부공기로
-                flag = True
-                maps[ni][nj] = 2
-                v[ni][nj] = 1
-                q.append((ni, nj))
-            if maps[ni][nj] == 2: # 이미 외부공기임
-                v[ni][nj] = 1
-                q.append((ni, nj))
-
-def cheese(i, j):
-    global melt, visit
-    q = deque()
-    q.append((i, j))
-
-    visit[i][j] = 1
-
-    while q:
-        ci, cj = q.popleft()
-        flag = False
-        for di, dj in dirs:
-            ni, nj = ci + di, cj + dj
             if visit[ni][nj] == 1:
                 continue
-            if maps[ni][nj] == 2:
-                # 공기와 만남
-                flag = True
-            elif maps[ni][nj] == 1:
-                # 치즈
+            if maps[ni][nj] == 0:
                 visit[ni][nj] = 1
                 q.append((ni, nj))
-        if flag:
-            melt.append((ci, cj))
-
-
-dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-ans = 0
+            else:
+                visit[ni][nj] = 1
+                maps[ni][nj] = 0
+                chz.append((ni, nj))
+        if not q:
+            if len(chz) > 0:
+                ans = len(chz)
+                time += 1
+                q = chz
+                chz = deque()
+            else:
+                return
 time = 0
-while True:
-    airs(0, 0)
-
-    visit = [[0] * M for _ in range(N)]
-    melt = deque()
-    for i in range(1, N-1):
-        for j in range(1, M-1):
-            if maps[i][j] == 1 and visit[i][j] == 0:
-                cheese(i, j)
-    cnt = len(melt)
-    if cnt > 0:
-        ans = cnt
-    else:
-        break
-
-    while melt:
-        mi, mj = melt.popleft()
-        maps[mi][mj] = 2
-    time += 1
-
+ans = 0
+bfs()
 print(time)
 print(ans)
