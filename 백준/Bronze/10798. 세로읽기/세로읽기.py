cc62913d3a65fdeed97811f22b7ec9282d5b1169
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-09-20 21:57:25 +0900

    [Bronze I] Title: 세로읽기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,16 @@
+a = input()
+b = input()
+c = input()
+d = input()
+e = input()
+for i in range(15):
+    if i < len(a):
+        print(a[i],end="")
+    if i < len(b):
+        print(b[i],end="")
+    if i < len(c):
+        print(c[i],end="")
+    if i < len(d):
+        print(d[i],end="")
+    if i < len(e):
+        print(e[i],end="")
