e83b6c68281fde9884fc1d19cc602710e5991201
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-09-22 03:30:43 +0900

    [Gold I] Title: 구간 합 구하기, Time: 1768 ms, Memory: 138660 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,43 +1,52 @@
 from math import ceil, log
 N, M, K = map(int, input().split())
-arr = [0] * N
-# h 트리의 높이, 노드의 개수 2 ** (h+1)
 h = ceil(log(N, 2))
 tree = [0] * (2 ** (h+1))
-leaf = {}
 for i in range(N):
-    tree[2**h + i] = int(input())
-    leaf[i] = 2**h + i
-# bot-up dp를 활용한 부분합 tree 만들기
-def trees():
+    tree[2 ** h + i] = int(input())
+
+def make_top_down(n):
     global tree
-    for i in range(2**h -1 , 0, -1):
-        # 부모노드는 자식노드 왼쪽 + 자식노드 오른쪽
+    if n >= (2**h):
+        return tree[n]
+    tree[n] = make_top_down(n*2) + make_top_down(n*2+1)
+    return tree[n]
+make_top_down(1)
+
+def make_bot_up():
+    global tree
+    for i in range(2**h-1, 0, -1):
         tree[i] = tree[i*2] + tree[i*2+1]
-def change(cur_node, diff):
-    global tree
-    if cur_node == 0:
-        return
-    tree[cur_node] += diff
-    change(cur_node//2, diff)
-def solve(l, r, node):
-    # b, c가 있으면
-    if c < l or r < b:
-        return 0
+#make_bot_up()
+def change(b, c):
+    # 리프노드부터 그 부모노드에 차이를 더해준다.
+    leaf = 2**h + b - 1
+    diff = c - tree[leaf]
+    # 부모노드 : leaf를 2로 나눈 몫
+    par = leaf
+    while par > 0:
+        tree[par] += diff
+        par //= 2
+def solve(l, r, node): # l, r은 해당범위
     if b <= l and r <= c:
+        # 탐색범위가 해당범위에 들어가면(작으면)
+        # 3 <= (3, 4) <= 5
         return tree[node]
+    if r < b or c < l:
+        # 탐색범위가 해당범위 바깥이면
+        # (1, 1) < 2, 5 < (7, 8)
+        return 0
+    # 탐색범위가 해당범위보다 크거나, 겹치면
+    # 이진탐색처럼 l, r 범위를 줄인다.
     mid = (l + r) // 2
-    ans = solve(l, mid, node*2) + solve(mid+1, r, node*2 + 1)
-    return ans
-trees()
+    return solve(l, mid, node*2) + solve(mid+1, r, node*2+1) #우측탐색 + 좌측탐색
 
 for _ in range(M+K):
     a, b, c = map(int, input().split())
     if a == 1:
-        # b번째 수를 c로 바꾸기
-        cur_node = leaf[b-1]
-        diff = c - tree[cur_node]
-        change(cur_node, diff)
+        change(b, c)
     else:
-        # b부터 c까지 구간합 구하기
-        print(solve(1, 2**h, 1))
+        # (b, c) 해당범위
+        # 바닥노드를 제외한 노드만 탐색
+        ret = solve(1, 2**h, 1)
+        print(ret)
