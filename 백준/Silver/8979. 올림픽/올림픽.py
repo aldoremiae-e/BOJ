8ce5a68da5ddd536e6da2bf740ccdcd818c2d49b
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-09-29 18:01:55 +0900

    [Silver V] Title: 올림픽, Time: 140 ms, Memory: 114844 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,21 +3,20 @@ names = [[0,0,0,0]]
 for _ in range(N):
     n, g, s, c = map(int, input().split())
     names.append([g, s, c, n])
-names.sort(key=lambda x:(-x[0], -x[1], -x[2]))
+names.sort(reverse=True)
 
-memo = {names[0][3]: 1}
-score = 1
-for i in range(1, N+1):
-    post = names[i-1]
-    cur = names[i]
-    for medal in range(3):
-        if post[medal] > cur[medal]:
-            score += 1
-            memo[cur[3]] = score
-            break
+score, same = 1, 0
+if names[0][3] == K:
+    print(score)
+else:
+    for i in range(1, N+1):
+        post = names[i-1]
+        cur = names[i]
+        if post[:3] == cur[:3]:
+            same += 1
         else:
-            continue
-    if cur[3] not in memo:
-        memo[cur[3]] = score
-        score += 1
-print(memo[K])
+            score += (same + 1)
+            same = 0
+        if cur[3] == K:
+            print(score)
+            break
