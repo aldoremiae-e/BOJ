5ad5e1cd6018b6fc51340aeda55766f830e98670
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-10-12 18:49:42 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,18 +1,9 @@
 def solution(targets):
     answer = 0
-    targets.sort()
-    targets.append([100_000_000, 100_000_000])
-    check = [-1, -1]
+    targets.sort(key=lambda x:x[1])
+    check = -1
     for target in targets:
-        if check[0] == -1 and check[1] == -1:
-            check[0] = target[0] + 0.1
-            check[1] = target[1] - 0.1
-            continue
-        if check[1] < target[0]:
+        if check <= target[0]:
             answer += 1
-            check[0] = target[0] + 0.1
-            check[1] = target[1] - 0.1
-        else:
-            check[0] = max(check[0], target[0]+0.1)
-            check[1] = min(check[1], target[1]-0.1)
+            check = target[1]
     return answer
