89cb56096d1f9d3a74af25ac7eca5f7dec35ebbf
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-10-12 16:48:33 +0900

    [Gold IV] Title: 오큰수, Time: 568 ms, Memory: 250760 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,19 +1,16 @@
-from collections import deque
 N = int(input())
 lst = list(map(int, input().split()))
-q = deque([])
-ans = [0 for _ in range(N)]
-for i in range(N-1, -1, -1):
-    if q:
-        while q:
-            if lst[i] < q[0]:
-                ans[i] = q[0]
-                break
-            else:
-                q.popleft()
-                ans[i] = -1
-        q.appendleft(lst[i])
+ans = [0] * N
+stack = []
+idx = N-1
+while lst:
+    cur = lst.pop()
+    while stack and stack[-1] <= cur:
+        stack.pop()
+    if not stack:
+        ans[idx] = -1
     else:
-        q.appendleft(lst[i])
-        ans[i] = -1
+        ans[idx] = stack[-1]
+    idx -= 1
+    stack.append(cur)
 print(*ans)
