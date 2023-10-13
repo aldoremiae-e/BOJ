35255700ea22398f4568ca56e527557d1dcf3edc
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-10-13 21:32:00 +0900

    [Gold V] Title: 로봇 청소기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,58 +1,36 @@
-from sys import stdin
-from collections import deque
-def clean(si, sj, sd, cnt):
-    global visit, ans, dir_z, dir_f, dir_s, dir_t, flag2
-
-    if ans < cnt:
-        ans = cnt
-    # 1
-    if visit[si][sj] == 0: # 청소되지 않은 칸
-        visit[si][sj] = cnt
-
-    flag = 0
-
-    if sd == 0:
-        dir = dir_z
-    elif sd == 1:
-        dir = dir_f
-    elif sd == 2:
-        dir = dir_s
-    elif sd == 3:
-        dir = dir_t
-
-    for di, dj, cd in dir: # 3 - 1반시계 방향으로 회전, 바라보는 방향
-        ci, cj = si + di, sj + dj # 앞쪽칸
-        if 0 <= ci < N and  0 <= cj < M and maps[ci][cj] != 1 and visit[ci][cj] == 0:
-            flag = 1
-            # 3 - 2
-            clean(ci, cj, cd, cnt+1)
-            if flag2 == 1:
-                return
-    # 2
-
-    if flag == 0:
-        # 2-1
-        if sd == 0 and 0 <= si+1 < N and maps[si+1][sj] != 1:
-            clean(si+1, sj, sd, cnt)
-        elif sd == 1 and 0 <= sj-1 < M and maps[si][sj-1] != 1:
-            clean(si, sj-1, sd, cnt)
-        elif sd == 2 and 0 <= si-1 < N and maps[si-1][sj] != 1:
-            clean(si-1, sj, sd, cnt)
-        elif sd == 3 and 0 <= sj+1 < M and maps[si][sj+1] != 1:
-            clean(si, sj+1, sd, cnt)
-        else:
-            # 2-2 : 모든 함수 자체를 다 끝내야 하기 때문.
-            flag2 = 1
-
-N, M = map(int, stdin.readline().split())
-ni, nj, direction = map(int, stdin.readline().split()) # 0북 1동 2남 3서
-maps = [list(map(int, stdin.readline().split())) for _ in range(N)]
+N, M = map(int, input().split())
+ci, cj, cd = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+cnt = 0
 visit = [[0] * M for _ in range(N)]
-ans = 0
-dir_z = [(0, -1, 3), (1, 0, 2), (0, 1, 1), (-1, 0, 0)]
-dir_f = [(-1, 0, 0), (0, -1, 3), (1, 0, 2), (0, 1, 1)]
-dir_s = [(0, 1, 1), (-1, 0, 0), (0, -1, 3), (1, 0, 2)]
-dir_t = [(1, 0, 2), (0, 1, 1), (-1, 0, 0), (0, -1, 3)]
-flag2 = 0
-clean(ni, nj, direction, 1)
-print(ans)
+while True:
+    # 청소
+    if board[ci][cj] == 0 and visit[ci][cj] == 0:
+        visit[ci][cj] = 1
+        cnt += 1
+    # 4방향 빈칸 탐색
+    flag = True #4칸 모두 청소되어있을 때
+    ni, nj, nd = ci, cj, cd
+    for _ in range(4): # 반시계로 돌아서, 찾으면 그곳으로 이동
+        nd = (nd - 1) % 4
+        ni, nj = ci + dirs[nd][0], cj + dirs[nd][1]
+        # 범위, 벽일 때 넘어가기
+        if not (0 <= ni < N and 0 <= nj < M):
+            continue
+        if board[ni][nj] == 1 or visit[ni][nj] == 1:
+            continue
+        if board[ni][nj] == 0 and visit[ni][nj] == 0:
+            flag = False
+            break
+    if flag:
+        # 후진
+        nd = (cd + 2) % 4
+        ni, nj = dirs[nd][0] + ci, dirs[nd][1] + cj
+        if board[ni][nj] == 1:
+            break
+        else:
+            ci, cj = ni, nj
+    else:
+        ci, cj, cd = ni, nj, nd
+print(cnt)
