63ecd5c3eb15c24ee85d52c22cbd989ff0646c72
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-10-17 19:40:01 +0900

    [Gold V] Title: 괄호의 값, Time: 120 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,43 @@
+'''
+(()[[]])([])
+'''
+b = list(input())
+stack = []
+temp = 1
+ans = 0
+for i in range(len(b)):
+    if b[i] == '(':
+        stack.append(b[i])
+        temp *= 2
+        #print(stack, temp, ans)
+
+    elif b[i] == '[':
+        stack.append(b[i])
+        temp *= 3
+        #print(stack, temp, ans)
+
+    elif b[i] == ')':
+        if not stack or stack[-1] == '[':
+            ans = 0
+            break
+        elif stack[-1] == '(':
+            stack.pop()
+            if b[i-1] == '(':
+                ans += temp
+            temp //= 2
+            #print(stack, temp, ans)
+
+    elif b[i] == ']':
+        if not stack or stack[-1] == '(':
+            ans = 0
+            break
+        elif stack[-1] == '[':
+            stack.pop()
+            if b[i - 1] == '[':
+                ans += temp
+            temp //= 3
+            #print(stack, temp, ans)
+
+if stack:
+    ans = 0
+print(ans)
