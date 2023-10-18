0c9599e5750ac62781b30aece803d26366ee1bc6
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-10-18 19:22:45 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 68 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -1,35 +1,31 @@
-from sys import stdin
 from collections import deque
-N, M = map(int, stdin.readline().split())
-mymap = [0] * 101
-for _ in range(N):
-    a, b = map(int, stdin.readline().split())
-    mymap[a] = b
-for _ in range(M):
-    c, d = map(int, stdin.readline().split())
-    mymap[c] = d
-
-#print(ladder, snake)
-
-q = deque()
-visit = [0] * 101
-visit[0] = 1
-def bfs(start):
-    visit[start] = 1
-    q.append(start)
+N, M = map(int, input().split())
+info = {}
+for _ in range(N+M):
+    a, b = map(int, input().split())
+    info[a] = b
+ans = int(1e9)
+def bfs():
+    global ans
+    q = deque()
+    q.append((0, 1))
+    visit = [False] * 101
+    visit[1] = True
     while q:
-        # 현재 위치
-        cur = q.popleft()
-        # 만약 현재위치에 사다리가 있다면
-        for i in range(1, 7):
-            if visit[cur + i] == 0:
-                if mymap[cur + i] != 0 and visit[mymap[cur + i]] == 0:
-                    visit[cur + i] = visit[cur] + 1
-                    visit[mymap[cur + i]] = visit[cur] + 1
-                    q.append(mymap[cur + i])
-                elif mymap[cur + i] == 0:
-                    visit[cur + i] = visit[cur] + 1
-                    q.append(cur + i)
-            if cur + i == 100:
-                return visit[100] - 1
-print(bfs(1))
+        cnt, cur = q.popleft()
+        if cur == 100:
+            ans = min(ans, cnt)
+            continue
+        if cur in info:
+            cur = info[cur]
+            visit[cur] = True
+        for i in range(6, 0, -1):
+            nxt = cur + i
+            if nxt > 100:
+                continue
+            if visit[nxt]:
+                continue
+            visit[nxt] = True
+            q.append((cnt+1, nxt))
+bfs()
+print(ans)
