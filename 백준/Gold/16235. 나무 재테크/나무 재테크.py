e3e52acedb54befcecee8877e6259d65a88b37ec
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-10-19 17:27:24 +0900

    [Gold III] Title: 나무 재테크, Time: 680 ms, Memory: 159388 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,46 +1,41 @@
-from collections import deque
-dirs = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+from heapq import heappop, heappush
 
-def ssfw():
-    global trees, maps
-    new_trees = deque()
-    five_trees = deque()
-    new_maps = [[0] * N for _ in range(N)]
-    for tree in trees:
-        stx, sty, told = tree[0], tree[1], tree[2]
-        if maps[stx][sty] >= told:
-            maps[stx][sty] -= told
-            new_trees.append((stx, sty, told+1))
-            if (told + 1) % 5 == 0:
-                five_trees.append((stx, sty))
-        else:
-            new_maps[stx][sty] += int(told // 2)
-    # 죽은 나무 양분이 됨
-    trees = new_trees
-    birth = deque()
-
-    for tree in five_trees:
-        ftx, fty = tree[0], tree[1]
-        for di, dj in dirs:
-            ci, cj = di + ftx, dj + fty
-            if 0 <= ci < N and 0 <= cj < N:
-                birth.append((ci, cj, 1))
-    for t in birth:
-        trees.appendleft(t)
-
-    # 양분 추가
-    for i in range(N):
-        for j in range(N):
-            maps[i][j] += (add_maps[i][j] + new_maps[i][j])
-    return
 N, M, K = map(int, input().split())
-maps = [[5] * N for _ in range(N)]
-add_maps = [list(map(int, input().split())) for _ in range(N)]
-trees = deque()
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [[5] * N for _ in range(N)]
+trees = [[[] for _ in range(N)] for _ in range(N)]
 for _ in range(M):
     x, y, z = map(int, input().split())
-    trees.append((x-1, y-1, z))
+    trees[x-1][y-1].append(z)
 
 for _ in range(K):
-    ssfw()
-print(len(trees))
+    birth = []
+    # 봄 + 가을
+    for i in range(N):
+        for j in range(N):
+            new_tree = []
+            trees[i][j].sort()
+            dead = 0
+            for z in trees[i][j]:
+                if B[i][j] >= z:
+                    B[i][j] -= z
+                    new_tree.append(z+1)
+                    if (z+1) % 5 == 0:
+                        for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+                            nx, ny = i + dx, j + dy
+                            if not (0 <= nx < N and 0 <= ny < N):
+                                continue
+                            birth.append((nx, ny))
+                else:
+                    dead += z//2
+            trees[i][j] = new_tree
+            B[i][j] += dead + A[i][j]
+
+    while birth:
+        i, j = birth.pop()
+        trees[i][j].append(1)
+ans = 0
+for i in range(N):
+    for j in range(N):
+        ans += len(trees[i][j])
+print(ans)
