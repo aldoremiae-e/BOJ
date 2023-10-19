d105f9fe038d68882708f5ca604bd86a406e05b0
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-10-19 15:05:21 +0900

    [Silver II] Title: 이동하기, Time: 724 ms, Memory: 70040 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -1,6 +1,8 @@
 N, M = map(int, input().split())
-maps = [[0] * (M+1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
+lst = [[0] * (M+1) for _ in range(N+1)]
+
 for i in range(1, N+1):
+    a = list(map(int, input().split()))
     for j in range(1, M+1):
-        maps[i][j] = max(maps[i-1][j], maps[i][j-1]) + maps[i][j]
-print(maps[N][M])
+        lst[i][j] = max(lst[i-1][j], lst[i][j-1]) + a[j-1]
+print(lst[N][M])
