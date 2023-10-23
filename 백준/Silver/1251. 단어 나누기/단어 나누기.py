f026ba15c003b745ac9772eb8ef8f11b7e073424
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-10-23 22:36:33 +0900

    [Silver V] Title: 단어 나누기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -0,0 +1,12 @@
+word=input()
+words=[]
+
+for i in range(1,len(word)):
+    A=word[:i][::-1]
+    for j in range(i, len(word)-1):
+        B=word[i:j+1][::-1]
+        C=word[j+1:][::-1]
+        words.append(A+B+C)
+        
+words.sort()
+print(words[0])
