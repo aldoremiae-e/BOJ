2684fcaac140eb8e9a0e4af5f75e8ee6dd868f1d
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-11-14 23:31:20 +0900

    [Bronze IV] Title: 뉴턴과 사과, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/13118. 뉴턴과 사과/뉴턴과 사과.py
@@ -0,0 +1,12 @@
+a, b, c, d = map(int, input().split())
+q, w, e = map(int, input().split())
+if a == q:
+    print(1)
+elif b == q:
+    print(2)
+elif c == q:
+    print(3)
+elif d == q:
+    print(4)
+else:
+    print(0)
