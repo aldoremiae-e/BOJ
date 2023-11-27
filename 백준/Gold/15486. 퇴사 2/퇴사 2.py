a43f7b26714a9dfe2b573a0a848d18d610b1b976
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-11-27 16:44:00 +0900

    [Gold V] Title: 퇴사 2, Time: 880 ms, Memory: 123640 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,14 +1,8 @@
 N = int(input())
-works = [(0, 0)]
-for _ in range(N):
-    works.append(tuple(map(int, input().split())))
-memo = [0 for _ in range(N+2)]
+lst = [0] * (N+2)
 for i in range(1, N+1):
-    memo[i] = max(memo[i], memo[i-1])
-    t, p = works[i][0], works[i][1]
-    next_day = i + t
-    if next_day <= N+1:
-        memo[next_day] = max(memo[next_day], p+memo[i])
-
-memo[N+1] = max(memo[N+1], memo[N])
-print(memo[N+1])
+    lst[i] = max(lst[i], lst[i-1])
+    t, p = map(int, input().split())
+    if i+t < N+2:
+        lst[i+t] = max(lst[i] + p, lst[i+t])
+print(max(lst[N], lst[N+1]))
