105ab5cd4630e9bfba24a2973070c49ebf077270
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-11-29 20:23:32 +0900

    [Bronze V] Title: Can you add this?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int t;
+long long x;
+long long y;
+int main(){
+   cin >> t;
+   for (int i=0; i<t; i++){
+       cin >> x >> y;
+       cout << x+y << '\n';
+   }
+}
