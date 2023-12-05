2fd192ad22a06ddcb7b30ef46d6159fb3bc28d7b
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-12-05 15:30:35 +0900

    [Silver I] Title: Z, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,32 +1,19 @@
-def conquer(N, r, c, cnt):
-    if N == 2:
-        # 숫자출력
-        if r == 0 and c == 0:
-            print(cnt)
-        elif r == 0 and c == 1:
-            print(cnt+1)
-        elif r == 1 and c == 0:
-            print(cnt+2)
-        elif r == 1 and c == 1:
-            print(cnt+3)
-        return
-    # (0, 0) (0, 4) (4, 0) (4, 4)
-    n = N//2
-    num = n**2
-    if r < n and c < n:
-        # 2사분면
-        conquer(n, r, c, cnt)
-    elif r < n and c >= n:
-        # 1사분면
-        conquer(n, r, c-n, cnt + num)
-    elif r >= n and c < n:
-        # 3사분면
-        conquer(n, r-n, c, cnt + num*2)
-    elif r >= n and c >= n:
-        # 4사분면
-        conquer(n, r-n, c-n, cnt + num*3)
+n, r, c = map(int, input().split())
+ans = 0
 
-N, r, c = map(int, input().split())
-# 맵을 줄이는 것이 아니고, 좌표(r, c)의 값을 줄이자
-# 첫번째값만 가져와
-conquer(2**N, r, c, 0)
+while n != 0:
+    n -= 1
+    N = 2 ** n
+    if r < N and c < N: # 2사분면
+        ans += 0
+    elif r < N and c >= N: #1사분면
+        ans += (N * N)
+        c -= N # N만큼 왼쪽으로
+    elif r  >= N and c < N: #3사분면
+        ans += (N * N) * 2
+        r -= N # N만큼 위쪽으로
+    else:
+        ans += (N * N) * 3
+        c -= N
+        r -= N
+print(ans)
