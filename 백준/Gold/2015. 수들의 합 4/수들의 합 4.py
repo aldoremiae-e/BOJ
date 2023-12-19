2ee245d529244bb213fb289f647a28f7b15b867c
miae <97646732+aldoremiae-e@users.noreply.github.com>
2023-12-19 13:50:30 +0900

    [Gold IV] Title: 수들의 합 4, Time: 152 ms, Memory: 135496 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
@@ -8,7 +8,7 @@ memo[0] = 1
 ans = 0
 for i in range(N):
     total += A[i]
-    # 현재까지의 누적합 dp[i] - dp[이전누적값] == k 이면 추가
+    # 현재까지의 누적합 total - memo[이전누적값] == k 이면 추가
     if total - K in memo:
         ans += memo[total - K]
     # 이전 누적값 메모이제이션
